ca529dfb891da169123937eb4f9ebd27bd7cb2dc
Pavlo Fedyna <fed.pavlo@gmail.com>
2022-09-20 00:10:11 +0200

    drop longhorn as not supported for 32bit arch. hook external NFS

--- monitoring/terraform/pvc.tf
+++ monitoring/terraform/pvc.tf
@@ -1,3 +1,22 @@
+resource "kubernetes_persistent_volume_v1" "grafana" {
+  metadata {
+    name      = "grafana"
+  }
+  spec {
+    access_modes = ["ReadWriteMany"]
+    capacity     = {
+      storage : "10Gi"
+    }
+    storage_class_name = "nfs"
+    persistent_volume_source {
+      nfs {
+        path   = "/k3s/grafana"
+        server = "192.168.178.51"
+      }
+    }
+  }
+}
+
 resource "kubernetes_persistent_volume_claim_v1" "grafana" {
   metadata {
     name      = "grafana"
@@ -7,9 +26,10 @@ resource "kubernetes_persistent_volume_claim_v1" "grafana" {
     access_modes = ["ReadWriteMany"]
     resources {
       requests = {
-        storage = "5Gi"
+        storage = "10Gi"
       }
     }
-    storage_class_name = "local-path"
+    volume_name = kubernetes_persistent_volume_v1.grafana.metadata[0].name
+    storage_class_name = "nfs"
   }
 }
