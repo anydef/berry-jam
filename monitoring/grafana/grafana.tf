7ac7f09ac6216f93323cc3b4158a759b0647ac9b
Pavlo Fedyna <fed.pavlo@gmail.com>
2023-03-17 16:28:44 +0100

    comment in how to clean up grafana non-versioned image name for grafana image argument

--- monitoring/grafana/grafana.tf
+++ monitoring/grafana/grafana.tf
@@ -4,5 +4,6 @@ module "grafana" {
   namespace       = var.namespace
   grafana_storage = var.grafana_storage
   grafana_name    = var.grafana_name
-  grafana_image   = var.grafana_image
+  grafana_image   = "${var.grafana_image}:latest"
+  github_container_registry_secret_name = var.github_container_registry_secret_name
 }
