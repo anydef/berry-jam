70a8c7223c5f26184b07b2c09b811055d58a9110
Pavlo Fedyna <fed.pavlo@gmail.com>
2023-03-18 01:00:53 +0100

    fix docker registry secretr

--- monitoring/secrets/github-container-registry-secret.tf
+++ monitoring/secrets/github-container-registry-secret.tf
@@ -3,7 +3,16 @@ resource "kubernetes_secret_v1" "dockerconfigjson" {
     name      = var.github_container_registry_secret_name
     namespace = var.namespace
   }
+  type = "kubernetes.io/dockerconfigjson"
+
   data = {
-    ".dockerconfigjson" = "{\"auths\":{\"ghcr.io\":{\"auth\":\"${var.github_container_registry_secret_value}\"}}}"
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "ghcr.io" = {
+          auth  = base64encode(var.github_container_registry_secret_value)
+        }
+      }
+    })
   }
 }
+
